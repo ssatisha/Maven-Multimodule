de082b0b8e352aa5a556479f95dcd76813733e0c
ssatisha <ssatisha@gmail.com>
2018-08-26 01:18:34 +0530

    Create Btest.java

---
+++ module-b/src/test/java/org/example/BTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+
+import org.junit.Test;
+
+
+
+public class BTest {
+
+
+
+    @Test
+
+    public void a() throws Exception {
+
+        new A().foo();
+
+    }
+
+
+
+}
